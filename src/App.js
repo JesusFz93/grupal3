b02c124bb58d09c81786505cd844802e5179cf8c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-08 12:42:16 -0600

    Formulario con imput nombre, usuario e email

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Formulario } from './components/Formulario'
 import { HomePage } from './pages/HomePage'
 
 const App = () => {
     return (
         <div>
             <HomePage />
+            <Formulario />
         </div>
     )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
+
 export const Formulario = () => {
     return (
-        <div>
-            
-        </div>
+            <form>
+                <div className="form-group">
+                    <label className="col-md-4 control-label">
+                        Nombre
+                    </label>
+                    <div className="col-md-5">
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            className="form-control input-md"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-4 control-label">
+                        Usuario
+                    </label>
+                    <div className="col-md-5">
+                        <input
+                            id="user"
+                            name="user"
+                            type="text"
+                            className="form-control input-md"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-4 control-label">
+                        Email
+                    </label>
+                    <div className="col-md-5">
+                        <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            className="form-control input-md"/>
+                    </div>
+                </div>
+            </form>
     )
 }
